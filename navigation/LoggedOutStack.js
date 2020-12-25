0024fc00cd8eb6ad50cc8e1d28870747f98f9a26
Vivi Ye <viviyyw0418@gmail.com>
2020-12-25 16:36:12 -0500

    navigation

--- navigation/LoggedOutStack.js
+++ navigation/LoggedOutStack.js
@@ -2,8 +2,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import * as React from "react";
 
-import startScreen from "../screens/signup/Start.js";
-
+import startScreen from "../screens/signup/Start";
+import SignIn from "../screens/signup/SignIn";
+import SignUp from "../screens/signup/SignUp";
 
 const Stack = createStackNavigator();
 export function LoggedOutStack() {
@@ -14,6 +15,9 @@ export function LoggedOutStack() {
       }}
     >
       <Stack.Screen name="start" component={startScreen} />
+      <Stack.Screen name="signup" component={SignUp} />
+      <Stack.Screen name="signin" component={SignIn} />
+
     </Stack.Navigator>
   );
 }
