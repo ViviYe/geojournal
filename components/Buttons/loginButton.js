08691ce28e19c2767623671ca962057931c0238d
Vivi Ye <viviyyw0418@gmail.com>
2020-12-25 16:15:28 -0500

    buttons

---
+++ components/Buttons/loginButton.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types'
+import {
+    Text,
+    Image,
+    StyleSheet,
+    View,
+    TouchableHighlight,
+    TouchableOpacity
+} from 'react-native'
+
+export default function LoginButton(props) {
+    const { text, color, background, onPress, icon, opacity, padding } = props
+    const backgroundColor = background || 'login from facebook'
+    const textColor = color || 'white'
+    const buttonOpacity = opacity || 0.5
+    const buttonPadding = padding || 15
+
+    return (
+        <TouchableOpacity
+            style={[styles.wrapper, { backgroundColor: backgroundColor, opacity: buttonOpacity, padding: buttonPadding }]}
+            onPress={onPress} >
+            <View style={styles.button}>
+                {icon}
+                <Text style={[styles.text, { color: color }]}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+
+LoginButton.propTypes = {
+    text: propTypes.string.isRequired,
+    color: propTypes.string.isRequired,
+    background: propTypes.string,
+    icon: propTypes.object,
+    onPress: propTypes.func.isRequired,
+    opacity: propTypes.number,
+    padding: propTypes.number,
+    //height: propTypes.oneOfType([propType.number, propType.string])
+}
+
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginBottom: 20,
+        display: 'flex',
+        borderWidth: 2,
+        borderRadius: 40,
+        borderColor: 'white',
+        justifyContent: 'center',
+        alignContent: 'center',
+        shadowColor: 'white',
+        shadowRadius: 10,
+        //shadowOpacity: 1,
+
+        shadowOffset: { width: 50, height: 10 }
+
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: '500'
+    },
+    button: {
+        flexDirection: 'row',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+
+})
--- screens/signup/Start.js
+++ screens/signup/Start.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
 import { StartScreenStyle } from "../../constants/Style";
+import LoginButton from "../../components/Buttons/loginButton";
+import Color from "../../constants/Colors";
 
 //path to logo
 //the shade of white we are using
@@ -11,10 +13,15 @@ const WHITE = "white";
  */
 export default function StartScreen({ navigation }) {
   return (
-    <View style={{backgroundColor: "#BCBFA7", flex: 1, padding: 20, alignItems:"center", paddingTop: "50%"}}>
+    <View style={{backgroundColor: "#BCBFA7", flex: 1, padding: 20, alignItems:"center", paddingTop: "60%"}}>
       
-      <Text style={{fontSize:40, color: "#F6F5D4", fontFamily: "Inter"}}>Sign Up</Text>
-     
+      <Text style={{fontSize:40, color: "#F6F5D4", fontFamily: "Roboto"}}>Sign Up</Text>
+      <LoginButton
+          onPress={() => navigation.push("signIn")}
+          color="black"
+          text="LOG IN"
+          background="#F6F5D4"
+        />
     </View>
   );
 }
