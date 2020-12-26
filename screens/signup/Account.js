9c9d8e245031b0253787f236e0c5b03c6d9ebfe2
Vivi Ye <viviyyw0418@gmail.com>
2020-12-26 02:28:47 -0500

    more screens

--- screens/signup/Account.js
+++ screens/signup/Account.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image, Text, SafeAreaView, ScrollView } from "react-native";
+import { View, Image, Text, SafeAreaView, ScrollView, FlatList, TouchableOpacity } from "react-native";
 import { StartScreenStyle } from "../../constants/Style";
 import LoginButton from "../../components/Buttons/loginButton";
 import Color from "../../constants/Colors";
@@ -8,14 +8,77 @@ import { Avatar } from 'react-native-elements';
 //path to logo
 //the shade of white we are using
 const WHITE = "white";
+const JournalDummy = [
+
+    {  
+        id: 1,
+        title: "First journal entry",
+        description: "today was a bad day, I did nothing, WAHHHH",
+        created_at: "2020-12-25",
+        longitude: -76.4735,
+        latitude: 42.4534,
+        address: "524 College Ave Cornell"
+    },
+    {  
+        id: 2,
+        title: "Second journal entry",
+        description: "mehhh",
+        created_at: "2020-12-25",
+        longitude: -76.4735,
+        latitude: 42.4534,
+        address: "120 Valentine Pl"
+    },
+    {
+        id: 3,
+        title: "Third journal entry",
+        description: "HEHEHEHE",
+        created_at: "2020-12-25",
+        longitude: -76.4735,
+        latitude: 42.4534,
+        address: "120 Valentine Pl"
+    },
+    {
+        id: 4,
+        title: "Forth journal entry",
+        description: "HEHEHEHE",
+        created_at: "2020-12-25",
+        longitude: -76.4735,
+        latitude: 42.4534,
+        address: "120 Valentine Pl"
+    }
+
+]
+
+const Card = ({ title, message, location, date }) => (
+    <View style={{backgroundColor: '#F6F5D4', width:'80%', alignSelf:'center', 
+    borderRadius:10, marginVertical:10, padding: 10
+    }}>
+        <Text style={{ fontSize: 15, color: "#575632", fontFamily:'Roboto', alignItems:'center'}}> {title} </Text>
+        <View style={{padding: 5}}>
+        <Text style={{ fontSize: 10, color: "#575632", fontFamily:'Roboto', alignItems:'center'}}>{message} </Text>
+        </View>
+        <View style={{alignItems:'flex-end'}}>
+        <Text style={{ fontSize: 10, color: "#575632", fontFamily:'Roboto', alignItems:'center'}}> Created at {location} </Text>
+        </View>
+        <View style={{alignItems:'flex-end'}}>
+        <Text style={{ fontSize: 10, color: "#575632", fontFamily:'Roboto', alignItems:'center'}}> on {date} </Text>
+        </View>
+     </View>
+  );
 
 /**
  * Start screen component
  */
 export default function StartScreen({ navigation }) {
+
+    const [friends, setFriends] = React.useState(0);
+    const renderItem = ({ item }) => (
+        <Card title={item.title} message={item.description} location={item.address} date={item.created_at} />
+      );
+
   return (
 
-    <ScrollView style={{backgroundColor: "#BCBFA7", flex: 1}}>
+    <SafeAreaView style={{backgroundColor: "#BCBFA7", flex: 1}}>
       <View style={{alignItems:"center", marginTop:'3%'}}>
       <Avatar
             size="large"
@@ -24,13 +87,24 @@ export default function StartScreen({ navigation }) {
               uri: "https://placekitten.com/200/300",
             }}
           />
-        <Text style={{ fontSize: 20, color: "#F6F5D4", fontFamily: "Roboto", marginVertical:'5%' }}>Name Placeholder</Text>
+        <Text style={{ fontSize: 20, color: "#F6F5D4", fontFamily: "Roboto", marginVertical:'5%' }}>Alanna Zhou</Text>
       </View>
-      <View style={{}}>
-
-      </View>
-      
-    </ScrollView>
+      <TouchableOpacity 
+      onPress={()=>navigation.push('friends')}
+      style={{backgroundColor: '#F6F5D4', height:90, width:'80%', 
+      alignSelf:'center', borderRadius:10, 
+      justifyContent:'center',
+   
+      }}>
+        <Text style={{alignSelf:"center", fontSize: 20, color: "#575632", fontFamily:'Roboto'}}> {friends} · Friends </Text>
+      </TouchableOpacity>
+      <FlatList
+        style={{marginTop: 20}}
+        data={JournalDummy}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   
   );
 }
---
+++ screens/signup/Friends.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { View, Image, Text, SafeAreaView, ScrollView, FlatList, StyleSheet, Modal, TouchableHighlight } from "react-native";
+import { StartScreenStyle } from "../../constants/Style";
+import LoginButton from "../../components/Buttons/loginButton";
+import Color from "../../constants/Colors";
+import { Avatar, Icon } from 'react-native-elements';
+
+//path to logo
+//the shade of white we are using
+const WHITE = "white";
+
+
+const Card = ({ name }) => (
+    <View style={{backgroundColor: '#F6F5D4', width:'80%', alignSelf:'center', 
+    borderRadius:10, marginVertical:10, padding: 10
+    }}>
+        <Text style={{ fontSize: 15, color: "black", fontFamily:'Roboto', alignItems:'center'}}> {name} </Text>
+       
+     </View>
+  );
+
+/**
+ * Start screen component
+ */
+export default function StartScreen({ navigation }) {
+
+    const [friends, setFriends] = React.useState(["yy453@cornell.edu", "asz33@cornell.edu", "yl2795@cornell.edu"]);
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const renderItem = ({ item }) => (
+        <Card name={item} />
+      );
+
+  return (
+
+    <SafeAreaView style={{backgroundColor: "#BCBFA7", flex: 1}}>
+
+
+
+<Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+
+    <View style={{padding:'10%', flexDirection:'row'}}>
+    <Text style={{ fontSize: 20, color: "#F6F5D4", fontFamily:'Roboto', alignItems:'center', marginRight: 10}}> Friends </Text>
+    <Icon
+    name='user-plus'
+    type='font-awesome-5'
+    onPress={()=> setModalVisible(true)}
+    ></Icon>
+    </View>
+      <FlatList
+        style={{marginTop: 20}}
+        data={friends}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5
+    },
+    openButton: {
+      backgroundColor: "#F194FF",
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
+  });
