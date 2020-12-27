df3a1467f6b14e53845b432a73d5d81ed1a79874
Vivi Ye <viviyyw0418@gmail.com>
2020-12-26 22:17:42 -0500

    add view location journal entries

--- screens/signup/Main.js
+++ screens/signup/Main.js
@@ -20,10 +20,100 @@ import { StartScreenStyle } from "../../constants/Style";
 import LoginButton from "../../components/Buttons/loginButton";
 import Color from "../../constants/Colors";
 import { Avatar, Icon } from "react-native-elements";
+import { FlatList } from "react-native-gesture-handler";
 
 //path to logo
 //the shade of white we are using
 const WHITE = "white";
+const JournalCard = ({ title, author, location, date }) => (
+  <View
+    style={{
+      backgroundColor: "#F4F1DE",
+      width: "100%",
+      alignSelf: "center",
+      borderRadius: 5,
+      marginVertical: 10,
+      padding: 10,
+      paddingHorizontal: 20,
+      shadowColor: "black",
+      shadowRadius: 4,
+      shadowOpacity: 0.25,
+      shadowOffset: { width: 0, height: 2 },
+    }}
+  >
+    
+    
+    <Text
+      style={{
+        fontSize: 15,
+        color: "#575632",
+        fontFamily: "RobotoMono-Regular",
+        fontWeight: "600",
+        alignItems: "center",
+      }}
+    >
+      {" "}
+      {title}{" "}
+    </Text>
+
+    <Text
+      style={{
+        fontSize: 12,
+        color: "#575632",
+        fontFamily: "RobotoMono-Regular",
+        alignItems: "center",
+        margin: 2,
+        marginVertical: 5,
+      }}
+    >
+      {" "}
+      {date}{" "}
+    </Text>
+  </View>
+);
+
+const JournalDummy = [
+  {
+    id: 1,
+    author: "person@cornell.edu",
+    title: "First journal entry",
+    description: "today was a bad day, I did nothing, WAHHHH",
+    created_at: "Dec 25, 2020",
+    longitude: -76.4735,
+    latitude: 42.4534,
+    address: "524 College Ave Cornell",
+  },
+  {
+    id: 2,
+    author: "person@cornell.edu",
+    title: "Second journal entry",
+    description: "mehhh",
+    created_at: "Dec 25, 2020",
+    longitude: -76.4735,
+    latitude: 42.4534,
+    address: "120 Valentine Pl",
+  },
+  {
+    id: 3,
+    author: "person@cornell.edu",
+    title: "Third journal entry",
+    description: "HEHEHEHE",
+    created_at: "Dec 25, 2020",
+    longitude: -76.4735,
+    latitude: 42.4534,
+    address: "120 Valentine Pl",
+  },
+  {
+    id: 4,
+    author: "person@cornell.edu",
+    title: "Forth journal entry",
+    description: "HEHEHEHE",
+    created_at: "Dec 25, 2020",
+    longitude: -76.4735,
+    latitude: 42.4534,
+    address: "120 Valentine Pl",
+  },
+];
 
 export const getCurrentLocation = () => {
   return new Promise((resolve, reject) => {
@@ -55,6 +145,18 @@ export default function Main({ navigation }) {
       }
     }, []);
   });
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => {setReadJournal(false);  navigation.push("journal-detail", item)}}>
+      <JournalCard
+        title={item.title}
+        message={item.description}
+        location={item.address}
+        date={item.created_at}
+        author={item.author}
+      />
+    </TouchableOpacity>
+  );
 
   const addEntry = () => {
     setWriteJournal(false);
@@ -150,7 +252,6 @@ export default function Main({ navigation }) {
                   >
                     <TextInput
                       multiline={true}
-                      inputAccessoryViewID={inputAccessoryViewID}
                       placeholder="write something..."
                       style={{
                         height: "100%",
@@ -196,15 +297,21 @@ export default function Main({ navigation }) {
             <View style={styles.centeredView}>
               <TouchableWithoutFeedback>
                 <View style={styles.modalView}>
-                  <Text
+                  {/* <Text
                     style={{
                       fontFamily: "Roboto",
                       fontSize: 20,
                       color: "#73715a",
                     }}
                   >
-                    Here renders the journal information
-                  </Text>
+                    
+                  </Text> */}
+                  <FlatList
+              style={{ paddingTop: 20, flex: 1}}
+            data={JournalDummy}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
                 </View>
               </TouchableWithoutFeedback>
             </View>
@@ -233,11 +340,14 @@ export default function Main({ navigation }) {
         <View
           style={{
             position: "absolute",
-            justifyContent: "center",
-            paddingHorizontal: "5%",
+            width: '100%',
+            justifyContent: "space-between",
+            paddingHorizontal: "10%",
             flexDirection: "row",
             zIndex: 100,
             paddingVertical: "15%",
+            
+            
           }}
         >
           <TouchableOpacity onPress={() => navigation.push("account")}>
@@ -246,7 +356,7 @@ export default function Main({ navigation }) {
               source={require("../../assets/user.png")}
             />
           </TouchableOpacity>
-          <View style={{ width: "78%" }} />
+         
           <TouchableOpacity onPress={() => navigation.push("journal")}>
             <Image
               style={{ width: 40, height: 40 }}
